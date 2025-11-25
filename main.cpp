59d349b0d77ff78c1095c6b4663537d566696918
ArautD <sergiosg6@yahoo.com>
2025-11-25 15:20:24 -0300

    feat: Adicao de menu e opcao para sair do programa

--- main.cpp
+++ main.cpp
@@ -23,48 +23,77 @@ double calculoRaiz(double a, double b, double c){
 }
 
 int main(){
+    int opcao;
     setlocale(LC_ALL, "pt_BR.UTF-8");
     system("chcp 65001 > nul");
     system("cls");
+
+    do{
+        system("cls");
+        cout << "\n==============================================\n" << endl;
+        cout << "TRATAMENTO DE EXCEPTIONS" << endl;
+        cout << "\n==============================================\n" << endl;
+
+        cout << "Selecione uma das opções e aproveite :) \n";
+        cout << "1) Iniciar\n";
+        cout << "0) Sair\n";
+        cout << "opção selecionada: ";
+        cin >> opcao;
+        cin.ignore();
     
-    while(true){
+        switch (opcao){
+            case 1:
+                double a, b, c;
+                system("cls");
+                cout << "Digite o valor de a: ";
+                cin >> a;
+                cout << "Digite o valor de b: ";
+                cin >> b;
+                cout << "Digite o valor de c: ";
+                cin >> c;
+
+                try{
+                    double resultado = calculoRaiz(a, b, c);
+
+                    if(isnan(resultado)) {
+                        double delta = b*b -4*a*c;
+                        double x1 = (-b + sqrt(delta)) / (2*a);
+                        double x2 = (-b - sqrt(delta)) / (2*a);
+                        cout << "A equação possui duas raizes reais: " << x1 << " e " << x2 << endl;
+                        system("pause");
+                    }else{
+                        cout << "A equação possui uma raiz real: " << resultado << endl;
+                        system("pause");
+                    }
+                }
+                catch (const invalid_argument& e){
+                    cout << "Erro " << e.what() << endl;
+                    cout << ">> Tente novamente "<< endl;
+                    system("pause");
+                }
+                catch (const runtime_error& e){
+                    cout << "Erro " << e.what() << endl;
+                    cout << ">> Tente novamente "<< endl;
+                    system("pause");
+                }
+                catch (...){
+                    cout << "Erro Desconhecido ???" << endl;
+                    cout << ">> Tente novamente "<< endl;
+                    system("pause");
+                }
+
+                break;
+            case 0:
+                system("cls");
+                cout << "Saindo..." << endl;
+                break;
+
+            default:
+                cout << "Opção Inválida!" << endl;
+        }
         
-        double a, b, c;
+        
+    } while (opcao != 0);
 
-        cout << "Digite o valor de a: ";
-        cin >> a;
-        cout << "Digite o valor de b: ";
-        cin >> b;
-        cout << "Digite o valor de c: ";
-        cin >> c;
-
-        try{
-            double resultado = calculoRaiz(a, b, c);
-
-            if(isnan(resultado)) {
-                double delta = b*b -4*a*c;
-                double x1 = (-b + sqrt(delta)) / (2*a);
-                double x2 = (-b - sqrt(delta)) / (2*a);
-                cout << "A equação possui duas raizes reais: " << x1 << " e " << x2 << endl;
-                system("pause");
-            }else{
-                cout << "A equação possui uma raiz real: " << resultado << endl;
-                system("pause");
-            }
-        }
-        catch (const invalid_argument& e){
-            cout << "Erro " << e.what() << endl;
-            cout << ">> Tente novamente "<< endl;
-        }
-        catch (const runtime_error& e){
-            cout << "Erro " << e.what() << endl;
-            cout << ">> Tente novamente "<< endl;
-        }
-        catch (...){
-            cout << "Erro Desconhecido ???" << endl;
-            cout << ">> Tente novamente "<< endl;
-        }
-
-    }
     return 0;
 }
